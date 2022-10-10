0137e4c093b70aa1ea86a53fde2b901044087095
developer-munir <devolopermunir@gmail.com>
2022-10-10 23:58:28 +0600

    added currect answer matching function

--- src/components/QuizUi.jsx
+++ src/components/QuizUi.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const QuizUi = ({ quiz }) => {
-    // console.log(quiz);
-    const { question, correctAnswer, options } = quiz;
-    return (
-      <div className="container mx-auto flex  justify-center">
-        <div className="md:w-3/4 md:p-5 mx-2 border my-2 rounded-xl p-2">
-          <h1 className='md:text-2xl text-lg font-serif'>{question}</h1>
-          {options.map((option) => (
-            <div className="flex align-middle p-3">
-              <input
-                type="radio"
-                name="radio-4"
-                className="radio radio-accent"
-                checked
-              />
-              <p className='ml-2 md:text-xl font-mono'>{option}</p>
-            </div>
-          ))}
-        </div>
+  // console.log(quiz.options);
+  const { question, correctAnswer, options } = quiz;
+    const handleQuiz = (e) => {
+        if (e === correctAnswer) {
+            alert('right');
+        } else {
+            alert('wrong')
+      }
+  };
+  return (
+    <div className="container mx-auto flex  justify-center">
+      <div className="md:w-3/4 w-full md:p-5 mx-2 border my-2 rounded-xl p-2">
+        <h1 className="md:text-2xl text-lg font-serif">{question}</h1>
+        {options.map((option, index) => (
+          <label key={index} className="flex align-middle p-3">
+            <input
+              type="radio"
+              name="quiz"
+              value={option.value}
+              key={index}
+              className="radio radio-accent mr-2"
+              defaultChecked={option.value === correctAnswer}
+              onChange={() => handleQuiz(option)}
+            />
+            {option}
+          </label>
+        ))}
       </div>
-    );
+    </div>
+  );
 };
 
 export default QuizUi;
