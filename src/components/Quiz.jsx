74939de5903513731b223062a8123b759a016111
developer-munir <devolopermunir@gmail.com>
2022-10-12 14:39:54 +0600

    added score feature

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import QuizUi from "./QuizUi";
 
 const Quiz = () => {
   const loadQuiz = useLoaderData();
+  const [rightCount, setRightCount] = useState(0);
+  const [wrongCount, setWrongCount] = useState(0);
   return (
     <div>
-      <h1 className="text-center text-2xl font-serif text-[#E6425E] my-5">
-        React Quizes
-      </h1>
+      <div className="flex flex-col text-center md:flex-row align-middle justify-center text-2xl font-serif text-[#E6425E] my-5">
+        <span className="mr-2 mt-3 lg:mt-0">
+          Quize Length : {loadQuiz?.data?.questions.length}
+        </span>
+        <span className="mr-2 mt-3 lg:mt-0">Right Answer :{rightCount}</span>
+        <span className="mr-2 mt-3 lg:mt-0">Wrong Answer :{wrongCount}</span>
+      </div>
       {loadQuiz?.data?.questions.map((quiz) => (
-        <QuizUi key={quiz?.id} quiz={quiz}></QuizUi>
+        <QuizUi
+          key={quiz?.id}
+          quiz={quiz}
+          setRightCount={setRightCount}
+          rightCount={rightCount}
+          wrongCount={wrongCount}
+          setWrongCount={setWrongCount}
+        ></QuizUi>
       ))}
     </div>
   );
--- src/components/QuizUi.jsx
+++ src/components/QuizUi.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import swal from 'sweetalert';
 import { EyeOffOutline } from "heroicons-react";
-const QuizUi = ({ quiz }) => {
+const QuizUi = ({ quiz, setRightCount, setWrongCount, rightCount,wrongCount }) => {
   const { question, correctAnswer, options } = quiz;
-    const handleQuiz = (e) => {
-        if (e === correctAnswer) {
-            swal("Good job!", "You are right!", "success");
-        } else {
-          swal('Your are wrong!!!');
-      }
+  const handleQuiz = (e) => {
+    if (e === correctAnswer) {
+      swal("Good job!", "You are right!", "success");
+      setRightCount(rightCount + 1);
+      
+    } else {
+      swal("Your are wrong!!!");
+      setWrongCount(wrongCount + 1);
+      
+    }
   };
   const handleRightQuiz = () => {
     swal("Tap button to see the right answer!!!").then((value) => {
       swal(`The correct answer is: ${correctAnswer}`);
     });
-  }
+  };
   return (
     <div className="container mx-auto flex  justify-center">
-      <div className="md:w-3/4 w-full md:p-5 mx-2 border my-2 rounded-xl p-2 shadow-lg border-[#E6425E]">
+      <div className="md:w-3/4 w-full md:p-5 mx-2 border my-2 rounded-xl p-2 shadow-lg bg-[#E6425E] text-[#CAD5E2]">
         <div className="flex justify-between align-middle">
-          <h1 className="md:text-2xl text-lg font-serif mb-2 text-[#E6425E]">
-            {question}
-          </h1>
+          <h1 className="md:text-2xl text-lg font-serif mb-2">{question}</h1>
           <p
             onClick={handleRightQuiz}
             className="cursor-pointer text-[#E6425E]"
@@ -33,14 +35,14 @@ const QuizUi = ({ quiz }) => {
         {options.map((option, index) => (
           <label
             key={index}
-            className="flex align-middle p-3 text-xl hover:bg-red-200 rounded-md font-mono cursor-pointer"
+            className="flex align-middle p-3 text-xl hover:bg-red-200 hover:text-black rounded-md font-mono cursor-pointer"
           >
             <input
               type="radio"
               name="quiz"
               value={option.value}
               key={index}
-              className=" checked:bg-red-500 cursor-pointer mr-2 mt-1 radio"
+              className="cursor-pointer mr-2 mt-1 radio bg-[#CAD5E2]"
               defaultChecked={option.value === correctAnswer}
               onChange={() => handleQuiz(option)}
             />
